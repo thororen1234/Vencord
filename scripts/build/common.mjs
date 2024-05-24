682072495c594fd326a6192d1910165f4b750605
cheesesamwich <cheesesamwich@proton.me>
2024-05-24 08:30:53 +0100

    add a few equicord plugins

--- scripts/build/common.mjs
+++ scripts/build/common.mjs
@@ -82,7 +82,7 @@ export const globPlugins = kind => ({
         });
 
         build.onLoad({ filter, namespace: "import-plugins" }, async () => {
-            const pluginDirs = ["plugins/_api", "plugins/_core", "plugins", "userplugins", "tobleroneplugins", "tobleroneplugins/_suncordplugins"];
+            const pluginDirs = ["plugins/_api", "plugins/_core", "plugins", "userplugins", "tobleroneplugins", "tobleroneplugins/_suncordplugins", "tobleroneplugins/_equicordplugins"];
             let code = "";
             let plugins = "\n";
             let i = 0;
