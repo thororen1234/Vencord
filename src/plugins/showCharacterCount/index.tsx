f4bef42ec7827a74209957b8a2c64778dcddc474
programminglaboratorys <107296738+programminglaboratorys@users.noreply.github.com>
2024-06-23 02:33:08 +0300

    adding the plugin in a clean branch

---
+++ src/plugins/showCharacterCount/index.tsx
@@ -0,0 +1,49 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+// import { disableStyle, enableStyle } from "@api/Styles";
+import definePlugin from "@utils/types";
+import { findByPropsLazy } from "@webpack";
+import { Text, UserStore } from "@webpack/common";
+
+const characterCountClass = findByPropsLazy("characterCount");
+
+
+export default definePlugin({
+    patches: [
+        {
+            find: ".CHARACTER_COUNT_OVER_LIMIT",
+            replacement: [
+                {
+                    match: /let/,
+                    replace:
+                        "return $self.CharacterCountComponent(arguments[0]);$&",
+                }
+            ],
+        },
+    ],
+    name: "ShowCharacterCount",
+    authors: [
+        {
+            name: "sadan",
+            id: 521819891141967883n,
+        }, {
+            name: "iamme",
+            id: 984392761929256980n,
+        }
+    ],
+    description: "Show your character count while typing.",
+    CharacterCountComponent({ textValue, className }: {
+        textValue: string;
+        className?: string;
+    }): JSX.Element {
+        return (
+            <Text variant="heading-sm/bold" className={[className, characterCountClass.characterCount, "vc-scc-character-count"].join(" ")} style={{ color: (UserStore.getCurrentUser().premiumType ? 4000 : 2000) < textValue.length ? "red" : void 0 }}>
+                {textValue.length}
+            </Text>
+        );
+    },
+});
