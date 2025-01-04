9158caf556552eff5a5cba4449cd2a78d23acafd
Cortex <126973723+verysillycat@users.noreply.github.com>
2025-01-04 13:40:15 -0600

    chore(fix): lint test

--- src/equicordplugins/Statsfm/index.tsx
+++ src/equicordplugins/Statsfm/index.tsx
@@ -1,3 +1,9 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 import { definePluginSettings } from "@api/Settings";
 import { EquicordDevs } from "@utils/constants";
 import { Logger } from "@utils/Logger";
